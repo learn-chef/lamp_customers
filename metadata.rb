328e8c4e32c45ea1c513f1d162d5736b4374315c
tpetchel <tpetchel@gmail.com>
2017-04-04 14:54:21 -0400

    Initial

--- metadata.rb
+++ metadata.rb
@@ -5,15 +5,7 @@ license 'all_rights'
 description 'Installs/Configures lamp_customers'
 long_description 'Installs/Configures lamp_customers'
 version '0.1.0'
+issues_url 'https://github.com/learn-chef/lamp_customers/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/learn-chef/lamp_customers' if respond_to?(:source_url)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/lamp_customers/issues' if respond_to?(:issues_url)
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/lamp_customers' if respond_to?(:source_url)
+depends 'lamp'
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,10 +7,23 @@
 require 'spec_helper'
 
 describe 'lamp_customers::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'When all attributes are default, on Ubuntu 14.04' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04') do |_node, server|
+        server.create_data_bag(
+          'passwords',
+          'mysql' => {
+            'id' => 'mysql',
+            'admin_password' => 'fakeadminpassword',
+            'root_password' => 'fakerootpassword'
+          }
+        )
+      end
       runner.converge(described_recipe)
+    end
+
+    before do
+      stub_command("mysql -h 127.0.0.1 -u db_admin -pfakeadminpassword -D my_company -e 'describe customers;'").and_return(0)
     end
 
     it 'converges successfully' do
